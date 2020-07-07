6b08b3e941ce17e2ec712b21705740592e55e72e
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2020-07-08 04:11:30 +0800

    Improved test structure.

--- format-package-json-file.test.js
+++ format-package-json-file.test.js
@@ -8,61 +8,144 @@ const formatPackageJSONFile = (
 	require( "./format-package-json-file.js" )
 );
 
-(
-	async	function( ){
-				strictAssert
-				.equal(
-					(
-						await	formatPackageJSONFile(
-									"./"
-								)
-					),
+const TEST_MODULE_DIRECTORY = (
+	async	function TEST_MODULE_DIRECTORY( ){
+				try{
+					strictAssert
+					.equal(
+						(
+							await	formatPackageJSONFile(
+										(
+											"./"
+										)
+									)
+						),
 
-					(
-						true
-					),
+						(
+							true
+						),
 
-					(
-						"Must return true"
-					)
-				);
+						(
+							[
+								"#test-module-directory;",
+
+								"test module directory;",
+								"must return true;"
+							]
+						)
+					);
+
+					return	(
+								true
+							);
+				}
+				catch( error ){
+					console
+					.error(
+						(
+							error
+						)
+					);
+
+					return	(
+								false
+							);
+				}
 			}
-)( );
+);
 
-(
-	async	function( ){
+const TEST_PACKAGE_JSON_FILE_AND_DEFAULT_PACKAGE_PROPERTY_LIST = (
+	async	function TEST_PACKAGE_JSON_FILE_AND_DEFAULT_PACKAGE_PROPERTY_LIST( ){
 				const fs = require( "fs" );
 
 				const fsAsync = fs.promises;
 
-				const PACKAGE_PROPERTY_LIST = (
+				const DEFAULT_PACKAGE_PROPERTY_LIST = (
 					require( "./package-property-list.constant.js" )
 				);
 
-				strictAssert
-				.equal(
-					(
-						Object
-						.keys(
-							JSON
-							.parse(
-								await	fsAsync
-										.readFile(
-											"./package.json"
+				try{
+					strictAssert
+					.equal(
+						(
+							Object
+							.keys(
+								(
+									JSON
+									.parse(
+										(
+											await	fsAsync
+													.readFile(
+														(
+															"./package.json"
+														)
+													)
 										)
+									)
+								)
 							)
+							.toString( )
+						),
+
+						(
+							DEFAULT_PACKAGE_PROPERTY_LIST
+							.toString( )
+						),
+
+						(
+							[
+								"#test-package-json-file-and-default-package-property-list;",
+
+								"test package json file and default package property list;",
+								"must be equal;"
+							]
 						)
-						.toString( )
-					),
+					);
 
-					(
-						PACKAGE_PROPERTY_LIST
-						.toString( )
-					),
+					return	(
+								true
+							);
+				}
+				catch( error ){
+					console
+					.error(
+						(
+							error
+						)
+					);
 
-					(
-						"Package property list must be equal"
-					)
+					return	(
+								false
+							);
+				}
+			}
+);
+
+(
+	async	function TEST_SCENE_BASIC( ){
+				console
+				.table(
+					[
+						{
+							"test": (
+								"test module directory"
+							),
+
+							"result": (
+								await	TEST_MODULE_DIRECTORY( )
+							)
+						},
+
+						{
+							"test": (
+								"test package json file and default package property list"
+							),
+
+							"result": (
+								await	TEST_PACKAGE_JSON_FILE_AND_DEFAULT_PACKAGE_PROPERTY_LIST( )
+							)
+						}
+					]
 				);
 			}
 )( );
