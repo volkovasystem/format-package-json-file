94218dbed4284f1fbb113347a7196d82a1705589
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2020-08-06 17:53:09 +0800

    Added property list shell parameter support.

--- format-package-json-file.run.js
+++ format-package-json-file.run.js
@@ -4,6 +4,10 @@ const MODULE_DIRECTORY_PATH_SHELL_PARAMETER = (
 	"--moduleDirectoryPath"
 );
 
+const PROPERTY_LIST_SHELL_PARAMETER = (
+	"--propertyList"
+);
+
 (
 	async	function runFormatPackageJSONFile( shellParameterList ){
 				"use strict";
@@ -44,10 +48,56 @@ const MODULE_DIRECTORY_PATH_SHELL_PARAMETER = (
 						)
 				);
 
+				const propertyList = (
+						(
+								(
+										shellParameterList
+										.includes(
+											(
+												PROPERTY_LIST_SHELL_PARAMETER
+											)
+										)
+									===	true
+								)
+						)
+					?	(
+							(
+								shellParameterList[
+									(
+										(
+											shellParameterList
+											.indexOf(
+												(
+													PROPERTY_LIST_SHELL_PARAMETER
+												)
+											)
+										)+1
+									)
+								]
+							)
+							.split(
+								(
+									/[\,\s\t\n\r]+/gm
+								)
+							)
+						)
+					:	(
+							undefined
+						)
+				);
+
 				return	(
 							await	formatPackageJSONFile(
 										(
 											moduleDirectoryPath
+										),
+
+										(
+											{
+												"propertyList": (
+													propertyList
+												)
+											}
 										)
 									)
 						);
